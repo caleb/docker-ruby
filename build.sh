1cc7aa3f97c117fcfa1090a0ca505d46199e4081
Caleb Land <caleb@land.fm>
2015-04-13 11:03:06 -0400

    Build and push 1.9 from the root build and push scripts

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ cd 2.2
 ./build.sh
 cd ..
 
+cd 1.9
+./build.sh
+cd ..
+
 docker tag -f docker.rodeopartners.com/ruby:2.2.1-jessie docker.rodeopartners.com/ruby:2
 docker tag -f docker.rodeopartners.com/ruby:2.2.1-jessie docker.rodeopartners.com/ruby:latest
--- push.sh
+++ push.sh
@@ -4,5 +4,9 @@ cd 2.2
 ./push.sh
 cd ..
 
+cd 1.9
+./push.sh
+cd ..
+
 docker push docker.rodeopartners.com/ruby:latest
 docker push docker.rodeopartners.com/ruby:2
