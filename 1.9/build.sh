5b154ff5ccc4232ea0ffee4f07e1160030285929
Caleb Land <caleb@land.fm>
2015-05-20 21:31:44 -0400

    use the right argument...

--- 1.9/build.sh
+++ 1.9/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NO_CACHE=${2:-false}
+NO_CACHE=${1:-false}
 
 docker build --no-cache=$NO_CACHE --tag=docker.rodeopartners.com/ruby:1.9.3-jessie .
 docker tag -f docker.rodeopartners.com/ruby:1.9.3-jessie docker.rodeopartners.com/ruby:1.9.3
--- 2.1/build.sh
+++ 2.1/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NO_CACHE=${2:-false}
+NO_CACHE=${1:-false}
 
 docker build --no-cache=$NO_CACHE --tag=docker.rodeopartners.com/ruby:2.1.6-jessie .
 docker tag -f docker.rodeopartners.com/ruby:2.1.6-jessie docker.rodeopartners.com/ruby:2.1.6
